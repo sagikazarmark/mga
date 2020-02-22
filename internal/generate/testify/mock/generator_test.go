43ce4843df415ba2465ba1389e0b079227080921
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-02-22 14:00:23 +0100

    Empty generator test

--- internal/generate/testify/mock/generator_test.go
+++ internal/generate/testify/mock/generator_test.go
@@ -17,21 +17,7 @@ func TestGenerate(t *testing.T) {
 	tests := []struct {
 		name string
 	}{
-		{
-			name: "service_with_struct",
-		},
-		{
-			name: "simple_service",
-		},
-		{
-			name: "todo",
-		},
-		{
-			name: "unnamed_param",
-		},
-		{
-			name: "pointer_message",
-		},
+
 	}
 
 	for _, test := range tests {
@@ -45,7 +31,7 @@ func TestGenerate(t *testing.T) {
 
 			pkg.NeedTypesInfo()
 
-			service := pkg.Types.Scope().Lookup("Service").Type().(*types.Named)
+			iface := pkg.Types.Scope().Lookup("Service").Type().(*types.Named)
 
 			file := File{
 				File: gentypes.File{
@@ -59,18 +45,15 @@ func TestGenerate(t *testing.T) {
 						Path: "app.dev/pkg/pkdriver",
 					},
 				},
-				EndpointSets: []EndpointSet{
+				Interfaces: []Interface{
 					{
-						Service: Service{
-							Object: service.Obj(),
-							Type:   service.Underlying().(*types.Interface),
-						},
-						WithOpenCensus: true,
+						Object: iface.Obj(),
+						Type:   iface.Underlying().(*types.Interface),
 					},
 				},
 			}
 
-			expected, err := ioutil.ReadFile(fmt.Sprintf("./testdata/generator/%s/endpoint/zz_generated.endpoint.go", test.name))
+			expected, err := ioutil.ReadFile(fmt.Sprintf("./testdata/generator/%s/endpoint/zz_generated.mock.go", test.name))
 			require.NoError(t, err)
 
 			actual, err := Generate(file)
@@ -79,148 +62,4 @@ func TestGenerate(t *testing.T) {
 			assert.Equal(t, string(expected), string(actual), "the generated code does not match the expected")
 		})
 	}
-}
-
-func TestGenerate_CustomModule(t *testing.T) {
-	pkgs, err := loader.LoadRoots("./testdata/generator/custom_module")
-	require.NoError(t, err)
-
-	pkg := pkgs[0]
-
-	pkg.NeedTypesInfo()
-
-	service := pkg.Types.Scope().Lookup("Service").Type().(*types.Named)
-
-	file := File{
-		File: gentypes.File{
-			HeaderText: `// Copyright 2020 Acme Inc.
-// All rights reserved.
-//
-// Licensed under "Only for testing purposes" license.
-`,
-			Package: gentypes.PackageRef{
-				Name: "pkgdriver",
-				Path: "app.dev/pkg/pkdriver",
-			},
-		},
-		EndpointSets: []EndpointSet{
-			{
-				Service: Service{
-					Object: service.Obj(),
-					Type:   service.Underlying().(*types.Interface),
-				},
-				ModuleName:     "path.to.custom_module",
-				WithOpenCensus: true,
-			},
-		},
-	}
-
-	expected, err := ioutil.ReadFile("./testdata/generator/custom_module/endpoint/zz_generated.endpoint.go")
-	require.NoError(t, err)
-
-	actual, err := Generate(file)
-	require.NoError(t, err)
-
-	assert.Equal(t, string(expected), string(actual), "the generated code does not match the expected")
-}
-
-func TestGenerate_MultipleServices(t *testing.T) {
-	pkgs, err := loader.LoadRoots("./testdata/generator/multiple_services")
-	require.NoError(t, err)
-
-	pkg := pkgs[0]
-
-	pkg.NeedTypesInfo()
-
-	service := pkg.Types.Scope().Lookup("Service").Type().(*types.Named)
-	otherService := pkg.Types.Scope().Lookup("OtherService").Type().(*types.Named)
-	another := pkg.Types.Scope().Lookup("Another").Type().(*types.Named)
-
-	file := File{
-		File: gentypes.File{
-			HeaderText: `// Copyright 2020 Acme Inc.
-// All rights reserved.
-//
-// Licensed under "Only for testing purposes" license.
-`,
-			Package: gentypes.PackageRef{
-				Name: "pkgdriver",
-				Path: "app.dev/pkg/pkdriver",
-			},
-		},
-		EndpointSets: []EndpointSet{
-			{
-				Service: Service{
-					Object: service.Obj(),
-					Type:   service.Underlying().(*types.Interface),
-				},
-				WithOpenCensus: true,
-			},
-			{
-				Service: Service{
-					Object: otherService.Obj(),
-					Type:   otherService.Underlying().(*types.Interface),
-				},
-				WithOpenCensus: true,
-			},
-			{
-				Service: Service{
-					Object: another.Obj(),
-					Type:   another.Underlying().(*types.Interface),
-				},
-				WithOpenCensus: true,
-			},
-		},
-	}
-
-	expected, err := ioutil.ReadFile("./testdata/generator/multiple_services/endpoint/zz_generated.endpoint.go")
-	require.NoError(t, err)
-
-	actual, err := Generate(file)
-	require.NoError(t, err)
-
-	assert.Equal(t, string(expected), string(actual), "the generated code does not match the expected")
-}
-
-func TestGenerate_ServiceError(t *testing.T) {
-	pkgs, err := loader.LoadRoots("./testdata/generator/service_error")
-	require.NoError(t, err)
-
-	pkg := pkgs[0]
-
-	pkg.NeedTypesInfo()
-
-	service := pkg.Types.Scope().Lookup("Service").Type().(*types.Named)
-
-	file := File{
-		File: gentypes.File{
-			HeaderText: `// Copyright 2020 Acme Inc.
-// All rights reserved.
-//
-// Licensed under "Only for testing purposes" license.
-`,
-			Package: gentypes.PackageRef{
-				Name: "pkgdriver",
-				Path: "app.dev/pkg/pkdriver",
-			},
-		},
-		EndpointSets: []EndpointSet{
-			{
-				Service: Service{
-					Object: service.Obj(),
-					Type:   service.Underlying().(*types.Interface),
-				},
-				WithOpenCensus: true,
-				ErrorStrategy:  "service",
-			},
-		},
-	}
-
-	expected, err := ioutil.ReadFile("./testdata/generator/service_error/endpoint/zz_generated.endpoint.go")
-	require.NoError(t, err)
-
-	actual, err := Generate(file)
-	require.NoError(t, err)
-
-	assert.Equal(t, string(expected), string(actual), "the generated code does not match the expected")
 }
